6e2210c6790c10698a65ccd6cde4f18eb3e55ff7
madev <dev@acgdraw.com>
2020-08-13 07:50:44 +0800

    windows log

--- start.bat
+++ start.bat
@@ -1,4 +1,7 @@
 @echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+echo "MoeART IPS Serveice is running in background ..."
+
 :loop
-node server.js
+node server.js 2>&1 > %~dp0\log\%dt%.log
 goto loop
